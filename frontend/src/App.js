fc431ee1f94081922d0e2b9e3ce523e3d04efadc
ZarinaAttaria <146745823+ZarinaAttaria@users.noreply.github.com>
2024-10-21 10:37:15 +0500

    login + routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,17 @@ import "./App.css";
 import Landingpage from "./pages/Landingpage";
 import Navbar from "./Navbar";
 import Login from "./Auth/Login";
+import { Routes, Route } from "react-router";
 
 function App() {
   return (
     <>
       <Navbar />
-      <Landingpage />
-      <Login />
+
+      <Routes>
+        <Route path="/" element={<Landingpage />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </>
   );
 }
--- frontend/src/Auth/Login.js
+++ frontend/src/Auth/Login.js
@@ -1,20 +1,42 @@
 import React from "react";
 import { setPassword, setUserName } from "../redux/authSlice";
 import { useDispatch, useSelector } from "react-redux";
+import axios from "axios";
+import "../styles/LoginStyles.css";
+
 const Login = () => {
   const userName = useSelector((state) => state.auth.userName);
   const password = useSelector((state) => state.auth.password);
 
   const dispatch = useDispatch();
+
   const handleUserNameChange = (e) => {
     dispatch(setUserName(e.target.value));
   };
+
   const handlePasswordChange = (e) => {
     dispatch(setPassword(e.target.value));
   };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(`http://localhost:8000/users/login`, {
+        userName,
+        password,
+      });
+      console.log("Login successful", response.data);
+      localStorage.setItem("token", response.data.token);
+    } catch (error) {
+      console.log(error.response?.data?.message || "Login Unsuccessful");
+      console.log("Login Error", error);
+    }
+  };
+
   return (
-    <div>
-      <form>
+    <div className="form-box">
+      <div className="header-text">Login Form</div>
+      <form onSubmit={handleSubmit}>
         <input
           type="text"
           placeholder="Enter Username"
@@ -25,7 +47,9 @@ const Login = () => {
           type="password"
           placeholder="Enter Password"
           onChange={handlePasswordChange}
+          value={password}
         />
+        <button type="submit">Login</button>
       </form>
     </div>
   );
